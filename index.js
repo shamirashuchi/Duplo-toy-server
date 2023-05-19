ae7f919d2a8c193192417fcfb71bb3f1ee9a19cf
shamirashuchi <shamirashuchi@gmail.com>
2023-05-19 22:45:51 +0600

    data go to the server

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+app.post('/toy', async(req,res)=>{
+    const newtoy = req.body;
+    console.log(newtoy);
+})
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
