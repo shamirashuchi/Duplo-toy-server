67e5815fcd5e34846a503dc4076e9c6f9842af92
shamirashuchi <shamirashuchi@gmail.com>
2023-07-26 13:26:46 +0600

    Your commit message here

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -25,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
 
 const toyCollection = client.db('toyDB').collection('toy');
 
